d02470a8beb2e49ed046e11156b5726d28551143
Alyson <adi9er@virginia.edu>
2017-10-25 22:20:23 -0400

    about us'

---
+++ hooscooking/about-us.php
@@ -0,0 +1,186 @@
+
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+<head>
+
+  <!-- Basic Page Needs
+  ================================================== -->
+  <meta charset="utf-8">
+  <title>About Us |  by OD</title>
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <!-- Mobile Specific Metas
+  ================================================== -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+  <!-- CSS
+  ================================================== -->
+  <link rel="stylesheet" href="stylesheets/base.css">
+  <link rel="stylesheet" href="stylesheets/skeleton.css">
+  <link rel="stylesheet" href="stylesheets/layout.css">
+    <link rel="stylesheet" href="stylesheets/flexslider.css">
+    <link rel="stylesheet" href="stylesheets/prettyPhoto.css">
+    
+    
+    <!-- CSS
+  ================================================== -->
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
+    <script src="js/jquery.flexslider-min.js"></script>
+    <script src="js/scripts.js"></script>
+
+  <!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+
+  <!-- Favicons
+  ================================================== -->
+  <link rel="shortcut icon" href="images/favicon.ico">
+  <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+
+</head>
+<body>
+
+
+
+  <!-- Primary Page Layout
+  ================================================== -->
+
+ <?php include("header.html"); ?>
+
+  <div class="container">
+    
+
+        
+        
+        <article class="twelve columns main-content">
+        <h1>Hoos Home Cooking?</h1>
+        
+       
+        
+        <p> It is our mission to provide a small slice of comfort during the school year, in what can be a tumultuous experience, while also reducing food waste. With busy schedules and heavy work loads hot, home cooked meal can be hard to come by. </p>
+
+        <p>Hoo’s Home Cooking shared marketplace allows you to be a cook, consumer, or even both! If you are preparing a meal and are anticipating multiple portions of leftovers you can easily avoid food waste by placing a notification on our marketplace. With our easy to use interface cooks can easily specify ingredients, flag major food allergens, specify price, indicate price and set a window for pickup time. All customers have to do is <strong>pay, pickup and enjoy </strong>their meal. </p>
+       
+        <div class="container">
+    
+    <div class="ten columns">
+        
+        <div class="flex-container">
+        <div class="flexslider">
+        <ul class="slides">
+      
+          <li><img src="images/food_trade.jpg" alt="community" align="middle"/> </li> 
+  
+        </ul>
+    </div>
+    </div>
+    </div>
+    </div>
+
+        <p>We hope to foster a <strong>community</strong> platform where students and faculty and share portions of their home cooked meal for a profit. A valid virginia.edu email is required for sign up, we hope our home cooked community reflects the integrity of the UVA community. </p>
+
+ <div class="container">
+    
+    <div class="ten columns">
+        
+        <div class="flex-container">
+        <div class="flexslider">
+        <ul class="slides">
+      
+          <li><img src="images/uva_community.jpg" alt="community" align="middle"/> </li> 
+  
+        </ul>
+    </div>
+    </div>
+    </div>
+    </div>
+
+<p>Food waste is a huge issue and Americans are global leaders in the production of food waste. The Hoos Cooking company helps to <strong>reduce food waste</strong> by allowing students to sell their leftovers</p> 
+  <div class="container">
+    
+    <div class="ten columns">
+        
+        <div class="flex-container">
+        <div class="flexslider">
+  <ul class="slides">
+    <li>
+      
+<img src="images/food_waste.png" alt="food waste" align="middle"/> </li>
+  
+  </ul>
+</div>
+</div>
+    </div>
+    </div>
+  
+
+ 
+
+        
+        </article>
+
+        <!-- End main Content -->
+        
+        
+        <aside class="four columns right-sidebar">
+        
+  
+        
+        <div class="sidebar-widget">
+        <h2>Testimonials</h2>
+        <blockquote class="testimonial no-after">
+     My go to when I'm feeling stressed! Nothing like a homecooked meal.
+     <cite>A Stressed Student</cite>
+     </blockquote>
+     
+     <blockquote class="testimonial no-after">
+     Easy way to make extra pocket change AND eliminate food waste!
+     <cite>A Enviornmental Entrepraneur</cite>
+     </blockquote>
+     
+ 
+        </aside>
+        <!-- End Right Sidebar -->
+    
+    </div>
+
+<footer>
+
+<div class="footer sixteen columns over">
+
+<div class="social footer-columns ">
+<h3 align="middle" > Hoos Cooking, a community marketplace for homecooked meals.</h3>
+
+
+
+</div>
+
+</div>
+
+<div id="footer-base">
+<div class="container">
+<div class="eight columns">
+<a href="http://www.opendesigns.org/design/icebrrrg/">Icebrrg Website Template</a> &copy; 2012
+</div>
+
+<div class="eight columns far-edge">
+Design by <a href="http://www.opendesigns.org">OD</a>
+</div>
+</div>
+</div>
+
+</footer>
+
+<!-- End Document
+================================================== -->
+
+<script src="js/jquery.prettyPhoto.js"></script>
+</body>
+</html>
+
---
+++ hooscooking/index.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+<head>
+
+  <!-- Basic Page Needs
+  ================================================== -->
+  <meta charset="utf-8">
+  <title>Hoos Cooking Home</title>
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <!-- Mobile Specific Metas
+  ================================================== -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+  <!-- CSS
+  ================================================== -->
+  <link rel="stylesheet" href="stylesheets/base.css">
+  <link rel="stylesheet" href="stylesheets/skeleton.css">
+  <link rel="stylesheet" href="stylesheets/layout.css">
+    <link rel="stylesheet" href="stylesheets/flexslider.css">
+    <link rel="stylesheet" href="stylesheets/prettyPhoto.css">
+    
+    <!-- CSS
+  ================================================== -->
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
+    <script src="js/jquery.flexslider-min.js"></script>
+    <script src="js/scripts.js"></script>
+
+  <!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+
+  <!-- Favicons
+  ================================================== -->
+  <link rel="shortcut icon" href="images/hat_icon.png">
+
+</head>
+<body class="wrap">
+
+
+
+  <!-- Primary Page Layout
+  ================================================== -->
+
+ <?php include("header.html"); ?>
+
+  <div class="container">
+    
+    <div class="sixteen columns">
+        
+        <div class="flex-container">
+        <div class="flexslider">
+  <ul class="slides">
+    <li>
+      <a href="#"><img src="images/food2.jpg" alt="food slide 1" /></a>
+      <div class="flex-caption" style="display:none;">
+        <h5><a href="#">Too lazy to cook?</a></h5>
+    
+        <p>Easily browse the marketplace to find a home cooked meal.</p>    
+  </div>
+    </li>
+    <li>
+      <a href="#"><img src="images/food3.jpg" alt="food slide 2" /></a>
+      <div class="flex-caption" style="display:none;">
+        <h5><a href="#">Share food with your community</a></h5>
+    
+        <p>Have leftovers? No problem. Submit a post to our marketplace to easily turn your extra food into extra cash.</p>   
+  </div>
+    </li>
+  </ul>
+</div>
+
+    
+
+
+</div>
+
+<div class="tagline">
+        <p>Welcome to <strong>Hoos Cooking</strong>, a community marketplace for homecooked meals.<br />
+        Simple. Satisfying. Easy-to-use.</p>
+        </div>
+
+    </div><!-- container -->
+        
+        <hr>
+        
+        <section class="container">
+        
+    <div class="one-half column">
+      <h3><i class="icon-heart rounded"></i> Cook</h3>
+      <p>Upload information about your meal. Set a pick up time. Get a profit!</p>
+    </div>
+    <div class="one-half column">
+      <h3><i class="icon-cog rounded"></i> Consumer</h3>
+      <p>Select a meal, enter payment and pick up your meal. A home cooked meal is only a few clicks away!</p>
+    </div>
+
+        
+        </section>
+        
+       
+    
+    </div>
+
+<footer>
+
+<div class="footer sixteen columns over">
+
+<div class="social footer-columns ">
+<h3 align="middle" > Hoos Cooking, a community marketplace for homecooked meals.</h3>
+
+
+
+</div>
+
+</div>
+
+<div id="footer-base">
+<div class="container">
+<div class="eight columns">
+<a href="http://www.opendesigns.org/design/icebrrrg/">Icebrrg Website Template</a> &copy; 2012
+</div>
+
+<div class="eight columns far-edge">
+Design by <a href="http://www.opendesigns.org">OD</a>
+</div>
+</div>
+</div>
+
+</footer>
+
+<!-- End Document
+================================================== -->
+
+<script src="js/jquery.prettyPhoto.js"></script>
+</body>
+</html>
+
